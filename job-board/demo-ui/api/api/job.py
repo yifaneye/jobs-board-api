c1f26788e7a867500fd55f634d05f0e2a2b94eea
Yifan Ai <ayfallen@hotmail.com>
2020-10-02 20:00:52 +1000

    Generate code from the OpenAPI specification

---
+++ job-board/demo-ui/api/api/job.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+from flask import request, g
+
+from . import Resource
+from .. import schemas
+
+
+class Job(Resource):
+
+    def get(self):
+
+        return [], 200, None
+
+    def post(self):
+        print(g.json)
+
+        return None, 201, None
---
+++ job-board/demo-ui/api/routes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
+from __future__ import absolute_import
+
+from .api.job import Job
+
+
+routes = [
+    dict(resource=Job, urls=['/job'], endpoint='job'),
+]
