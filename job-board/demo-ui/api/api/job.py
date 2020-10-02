1c149fa09773158e5589e9d8a4c168cca6528631
Yifan Ai <ayfallen@hotmail.com>
2020-10-02 20:15:37 +1000

    Implement GET and POST handling

--- job-board/demo-ui/api/api/job.py
+++ job-board/demo-ui/api/api/job.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
 
+import csv
+
 from flask import request, g
 
 from . import Resource
@@ -8,12 +10,17 @@ from .. import schemas
 
 
 class Job(Resource):
+    DB = 'jobs.csv'
 
     def get(self):
-
-        return [], 200, None
+        with open(self.DB, 'r') as csvFile:
+            csvReader = csv.DictReader(csvFile)
+            response = [line for line in csvReader]
+        return response, 200, None
 
     def post(self):
-        print(g.json)
-
-        return None, 201, None
+        row = list(g.json.values())  # or [*g.json.values()]
+        with open(self.DB, 'a') as csvFile:
+            csvWriter = csv.writer(csvFile)
+            csvWriter.writerow(row)
+        return row, 201, None
